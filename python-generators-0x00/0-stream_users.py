27149d3d187eddfaba1e7e75a6fdb5f23de3885d
Haile-08 <haile4cmd@gmail.com>
2024-12-08 22:24:59 +0300

    Create a generator that streams rows from an SQL database one by one

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+from mysql.connector import Error
+
+seed = __import__('seed')
+
+def stream_users():
+    """
+    A generator function that yields user data from the user_data table in the ALX_prodev database.
+    """ 
+    try:
+        with seed.connect_to_prodev() as connection:
+            if connection and connection.is_connected():
+                with connection.cursor(dictionary=True, buffered=True) as cursor:
+                        cursor.execute("SELECT user_id, name, email, age FROM user_data;")                        
+                        for row in cursor:
+                            yield { f"user_id: {row['user_id']}, name: {row['name']}, email: {row['email']}, age: {row['age']}" }                   
+                        
+            else:
+                raise ValueError("Failed to connect to ALX_prodev database.")
+    
+    except TypeError as e:
+            print(f"Error connecting to ALX_prodev: {e}")
+            return None
+
+    except Error as e:
+        print(f"Error connecting to ALX_prodev: {e}")
+        return None
+    finally:
+        if connection and connection.is_connected():
+            connection.close()
--- python-generators-0x00/seed.py
+++ python-generators-0x00/seed.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import mysql.connector 
 from dotenv import load_dotenv
 import os
