81da84afac4e09a58f26cddce35428b9bceb39b5
Haile-08 <haile4cmd@gmail.com>
2024-12-14 19:54:33 +0300

    Add logging

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,16 +1,20 @@
 import sqlite3
 import functools
+import logging
+from datetime import datetime
 
-#### decorator to lof SQL queries
+logging.basicConfig(level=logging.INFO, filename='0-log_queries.log', filemode='a',
+    format='%(asctime)s - %(levelname)s - %(message)s')
+
+#### decorator to log SQL queries
 
 def log_queries(func):
-    """
-    Logs database queries executed by any function
-    """
-    def wrapper_log_queries(*args, **kwargs):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
         print(f"{datetime.now()} - Executing query: {kwargs.get('query')}")
+        logging.info(f"Executing query: {kwargs.get('query')}")
         return func(*args, **kwargs)
-    return wrapper_log_queries
+    return wrapper
 
 @log_queries
 def fetch_all_users(query):
