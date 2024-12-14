9adb9f606c215dbf9a53cc24ee4fba8a1f1891e1
Haile-08 <haile4cmd@gmail.com>
2024-12-14 18:35:31 +0300

    fix the stream function

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -9,15 +9,19 @@ def stream_users():
     A generator function that yields user data from the user_data table in the ALX_prodev database.
     """ 
     try:
-        with seed.connect_to_prodev() as connection:
-            if connection and connection.is_connected():
-                with connection.cursor(dictionary=True, buffered=True) as cursor:
-                        cursor.execute("SELECT user_id, name, email, age FROM user_data;")                        
-                        for (user_id, name, email, age) in cursor:
-                            yield { f"user_id: {user_id}, name: {name}, email: {email}, age: {age}" }                   
-                        
-            else:
-                raise ValueError("Failed to connect to ALX_prodev database.")
+        connection = seed.connect_to_prodev()
+        if connection and connection.is_connected():
+            cursor = connection.cursor(dictionary=True, buffered=True)
+            cursor.execute("SELECT user_id, name, email, age FROM user_data;")                        
+            for row in cursor:
+                yield {
+                    "user_id": row["user_id"],
+                    "name": row["name"],
+                    "email": row["email"],
+                    "age": row["age"]
+                }       
+        else:
+            raise ValueError("Failed to connect to ALX_prodev database.")
     
     except TypeError as e:
             print(f"Error connecting to ALX_prodev: {e}")
@@ -29,3 +33,4 @@ def stream_users():
     finally:
         if connection and connection.is_connected():
             connection.close()
+
