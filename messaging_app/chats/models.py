a7c647955450c52bacf59a6f982265368b55595c
Haile-08 <haile4cmd@gmail.com>
2025-01-21 15:54:57 +0300

    Create a nuser model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,33 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+# Create a user model here.
+class User(models.Model):
+    ROLE_CHOICES = {
+        'guest': 'guest',
+        'host': 'host',
+        'admin': 'admin',
+    }
+
+    user_id = models.UUIDField(
+                primary_key=True,
+                default=uuid.uuid4,
+                unique=True,
+                db_index=True
+            )
+    first_name = models.CharField(max_length=255, null=False)
+    last_name = models.CharField(max_length=255, null=False)
+    email = models.CharField(null=False, unique=True)
+    password_hash = models.CharField(max_length=255, null=False)
+    phone_number = models.CharField(null=True, blank=True)
+    role = models.CharField(
+                max_length=10,
+                choices=ROLE_CHOICES,
+                null=False,
+                default='guest'
+            )
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"- {self.first_name} {self.last_name}: ({self.email})"
