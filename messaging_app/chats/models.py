813963547bd13411c628a0841d4f95c4608d4419
Haile-08 <haile4cmd@gmail.com>
2025-01-22 19:41:06 +0300

    Create a conversation model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -33,3 +33,26 @@ class User(models.Model):
         return f"- {self.first_name} {self.last_name}: ({self.email})"
 
 
+# Create a message model here.
+class Message(models.Model):
+    message_id = models.UUIDField(
+                primary_key=True,
+                default=uuid.uuid4,
+                unique=True,
+                db_index=True
+            )
+    sender_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    message_body = models.TextField(null=False)
+    sent_at = models.DateField(auto_now_add=True)
+
+
+# Create a conversation model here.
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(
+                    primary_key=True,
+                    default=uuid.uuid4,
+                    unique=True,
+                    db_index=True
+                )
+    participants_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
