2381826bb2744c15f4f5f2330e5acebad46e5581
Haile-08 <haile4cmd@gmail.com>
2025-02-14 10:20:36 +0300

    Fix the serializer

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -4,11 +4,10 @@ from .models import User, Message, Conversation
 
 # User Serializer
 class UserSerializer(serializers.ModelSerializer):
-    full_name = serializers.CharField(source='get_full_name', read_only=True)
 
     class Meta:
         model = User
-        fields = ['user_id', 'first_name', 'last_name', 'email', 'phone_number', 'role', 'created_at', 'full_name']
+        fields = '__all__'
 
 
 # Message Serializer
@@ -17,13 +16,35 @@ class MessageSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Message
-        fields = ['message_id', 'sender_id', 'sender_name', 'conversation', 'message_body', 'sent_at']
+        fields = [
+            'message_id',
+            'sender_id',
+            'sender_name',
+            'conversation',
+            'message_body',
+            'sent_at'
+            ]
+
+    def get_sender_name(self, obj):
+        return f"{obj.sender_id.first_name} {obj.sender_id.last_name}"
 
 
 # Conversation Serializer
 class ConversationSerializer(serializers.ModelSerializer):
-    participants = UserSerializer(many=True)
-    messages = MessageSerializer(many=True, source='messages')
+    participants = serializers.SerializerMethodField()
+    messages = MessageSerializer(many=True, read_only=True,)
+
     class Meta:
         model = Conversation
-        fields = ['conversation_id', 'participants', 'messages', 'created_at']
+        fields = [
+            'conversation_id',
+            'participant_id',
+            'messages',
+            'created_at'
+            ]
+
+    def get_participants(self, obj):
+        return [
+                f"{participant.first_name} {participant.last_name}"
+                for participant in obj.participant_id.all()
+            ]
