31b59a96659d44d11db773a7e931e07acca89acb
Haile-08 <haile4cmd@gmail.com>
2025-02-14 13:15:27 +0300

    Create the conversation viewset

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
 from .models import User, Conversation 
@@ -17,7 +18,14 @@ class ConversationViewSet(viewsets.ViewSet):
     """
     A viewset for managing the conversation
     """
-    def list_conversation(self, request):
+    def list(self, request):
         queryset = Conversation.objects.all()
         serializer = ConversationSerializer(queryset, many=True)
         return Response(serializer.data)
+    
+    def create(self, request):
+        serializer = ConversationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
