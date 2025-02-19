db88da9f19a10866e2820215c16200784a80df61
Haile-08 <haile4cmd@gmail.com>
2025-02-19 09:34:42 +0300

    Create a nested route for message

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -5,7 +5,10 @@ from .views import UserViewSet, ConversationViewSet, MessageViewSet
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 router.register(r'conversation', ConversationViewSet, basename='conversation')
-router.register(r'message', MessageViewSet, basename='message')
+
+# create a nested route for message
+nested_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
+nested_router.register(r'message', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
     path('chat/', include(router.urls))
